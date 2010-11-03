583da4cd125124fb14d6dbf27c44e2b1acbd0606
Lucas Schnorr <Lucas.Schnorr@imag.fr>
2010-11-03 10:12:19 +0100

    README file and licensing under GPLv3

--- NSPointFunctions.h
+++ NSPointFunctions.h
@@ -1,18 +1,18 @@
 /*
-    This file is part of Triva.
+    This file is part of ForceDirected.
 
-    Triva is free software: you can redistribute it and/or modify
+    ForceDirected is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Triva is distributed in the hope that it will be useful,
+    ForceDirected is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Triva.  If not, see <http://www.gnu.org/licenses/>.
+    along with ForceDirected.  If not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef __NSPointFunctions_H_
 #define __NSPointFunctions_H_
